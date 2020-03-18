55ce32437f2ae431582d4b809ef5ddc4437f2373
m.coder <kurokoshow00@gmail.com>
2020-03-18 14:11:13 +0900

    add ArticleController

---
+++ src/main/kotlin/com/nanaten/SpringBootDemo/controller/ArticleController.kt
@@ -0,0 +1,30 @@
+package com.nanaten.SpringBootDemo.controller
+
+import com.nanaten.SpringBootDemo.domain.entity.Article
+import com.nanaten.SpringBootDemo.domain.repository.ArticleRepository
+import com.nanaten.SpringBootDemo.request.ArticleRequest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller
+class ArticleController {
+    @Autowired
+    lateinit var articleRepository: ArticleRepository
+
+    @PostMapping("/")
+    @ResponseBody
+    fun registerArticle(@ModelAttribute articleRequest: ArticleRequest): String {
+        val article = Article(
+                articleRequest.id,
+                articleRequest.name,
+                articleRequest.title,
+                articleRequest.contents,
+                articleRequest.articleKey
+        )
+        articleRepository.save(article)
+        return "Saved"
+    }
+}
