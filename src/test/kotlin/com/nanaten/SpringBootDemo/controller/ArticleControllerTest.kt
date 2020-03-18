15596e0e5a553663a79d5101adee548c146eab16
m.coder <kurokoshow00@gmail.com>
2020-03-18 14:11:25 +0900

    add ArticleControllerTest

---
+++ src/test/kotlin/com/nanaten/SpringBootDemo/controller/ArticleControllerTest.kt
@@ -0,0 +1,43 @@
+package com.nanaten.SpringBootDemo.controller
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+
+@SpringBootTest
+internal class ArticleControllerTest {
+
+    lateinit var mockMvc: MockMvc
+
+    @Autowired
+    lateinit var articleController: ArticleController
+
+    @BeforeEach
+    fun setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(articleController).build()
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun registerArticle() {
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/")
+                                .param("name", "test")
+                                .param("title", "test")
+                                .param("contents", "test")
+                                .param("articleKey", "test")
+                )
+                .andExpect(status().isOk)
+                .andExpect(content().string("Saved"))
+    }
+}
