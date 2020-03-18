a3c0f0c9ca3cde8550f399a2fd02b7c56d0b0196
m.coder <kurokoshow00@gmail.com>
2020-03-18 14:11:35 +0900

    add ArticleRequest

---
+++ src/main/kotlin/com/nanaten/SpringBootDemo/request/ArticleRequest.kt
@@ -0,0 +1,9 @@
+package com.nanaten.SpringBootDemo.request
+
+data class ArticleRequest(
+        var id: Int = 0,
+        var name: String = "",
+        var title: String = "",
+        var contents: String = "",
+        var articleKey: String = ""
+)
