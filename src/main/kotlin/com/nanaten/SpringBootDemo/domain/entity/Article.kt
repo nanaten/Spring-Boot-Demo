de092ed290b5ac929ef1ac155fa0a717ca60e10a
m.coder <kurokoshow00@gmail.com>
2020-03-18 14:10:53 +0900

    add Article

---
+++ src/main/kotlin/com/nanaten/SpringBootDemo/domain/entity/Article.kt
@@ -0,0 +1,21 @@
+package com.nanaten.SpringBootDemo.domain.entity
+
+import java.util.*
+import javax.persistence.*
+
+@Entity
+@Table(name = "articles")
+data class Article(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        var id: Int = 0,
+        var name: String = "",
+        var title: String = "",
+        var contents: String = "",
+        @Column(name = "article_key")
+        var articleKey: String = "",
+        @Column(name = "register_at")
+        var registerAt: Date = Date(),
+        @Column(name = "update_at")
+        var updateAt: Date = Date()
+)
