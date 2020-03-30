c68e13c4bd3d12de57f54d6582893f18884f43f2
m.coder <kurokoshow00@gmail.com>
2020-03-30 14:26:13 +0900

    Fix Security Rule

--- src/main/kotlin/com/nanaten/SpringBootDemo/config/BbsAdminWebSecurityConfig.kt
+++ src/main/kotlin/com/nanaten/SpringBootDemo/config/BbsAdminWebSecurityConfig.kt
@@ -5,10 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +22,25 @@ class BbsAdminWebSecurityConfig : WebSecurityConfigurerAdapter() {
     @Bean
     fun passwordEncoder(): PasswordEncoder {
         return BCryptPasswordEncoder()
+    }
+
+    override fun configure(web: WebSecurity) {
+        // ここに設定したものはセキュリティ設定を無視
+        web.ignoring().antMatchers(
+                "/favicon.ico", "/css/**", "/js/**"
+        )
+    }
+
+    override fun configure(http: HttpSecurity) {
+        // 許可の設定
+        http.authorizeRequests()
+                // /admin と/admin/配下は認証が必要
+                .antMatchers("/admin", "/admin/*").authenticated().anyRequest().permitAll() // それ以外は全てアクセス許可
+        // ログイン設定 (デフォルトの設定を用いる)
+        http.formLogin()
+        // ログアウト
+        http.logout()
+                .logoutRequestMatcher(AntPathRequestMatcher("/logout**")).logoutSuccessUrl("/")
     }
 
     override fun configure(auth: AuthenticationManagerBuilder?) {
