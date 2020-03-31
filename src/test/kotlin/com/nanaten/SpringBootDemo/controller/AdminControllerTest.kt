8952070cae5f40ba44405327e798a4da691d83d9
m.coder <kurokoshow00@gmail.com>
2020-03-31 15:08:26 +0900

    Add AdminControllerTest

---
+++ src/test/kotlin/com/nanaten/SpringBootDemo/controller/AdminControllerTest.kt
@@ -0,0 +1,36 @@
+package com.nanaten.SpringBootDemo.controller
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+
+@SpringBootTest
+@AutoConfigureMockMvc
+internal class AdminControllerTest {
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Autowired
+    lateinit var adminController: AdminController
+
+    @Test
+    fun noAuthenticationTest() {
+        mockMvc.perform(MockMvcRequestBuilders.get("/admin/index"))
+                .andExpect(status().is3xxRedirection)
+                .andExpect(redirectedUrlPattern("**/login"))
+    }
+
+    @Test
+    @WithMockUser(username = "admin")
+    fun authenticationTest() {
+        mockMvc.perform(MockMvcRequestBuilders.get("/admin/index"))
+                .andExpect(status().isOk)
+                .andExpect(model().attributeExists("page"))
+                .andExpect(view().name("admin_index"))
+    }
+}
