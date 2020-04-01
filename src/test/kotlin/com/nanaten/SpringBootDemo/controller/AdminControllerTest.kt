02062273160d5d85d1f9ffef026638f616f02e69
m.coder <kurokoshow00@gmail.com>
2020-04-01 16:56:11 +0900

    Add Delete Article Tests for Admin

--- src/test/kotlin/com/nanaten/SpringBootDemo/controller/AdminControllerTest.kt
+++ src/test/kotlin/com/nanaten/SpringBootDemo/controller/AdminControllerTest.kt
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
+import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
@@ -33,5 +35,32 @@ internal class AdminControllerTest {
                 .andExpect(model().attributeExists("page"))
                 .andExpect(view().name("admin_index"))
                 .andExpect(model().attributeExists("isAdmin"))
+    }
+
+    @Test
+    @WithMockUser(username = "admin")
+    fun singleDeleteNotExistsArticle() {
+        mockMvc.perform(MockMvcRequestBuilders.post("/admin/article/delete/0")
+                .with(csrf())
+        )
+                .andExpect(status().is3xxRedirection)
+                .andExpect(view().name("redirect:/admin/index"))
+                .andExpect(flash().attributeExists(adminController.MESSAGE))
+                .andExpect(flash().attribute(adminController.MESSAGE, adminController.MESSAGE_ARTICLE_DOES_NOT_EXISTS))
+    }
+
+    @Test
+    @Sql(statements = ["INSERT INTO articles (name, title, contents, article_key) VALUES ('test', 'test', 'test', 'test');"])
+    @WithMockUser(username = "admin")
+    fun singleDeleteExistsArticle() {
+        val latestArticle = adminController.articleRepository.findAll().last()
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/admin/article/delete/" + latestArticle.id)
+                .with(csrf())
+        )
+                .andExpect(status().is3xxRedirection)
+                .andExpect(view().name("redirect:/admin/index"))
+                .andExpect(flash().attributeExists(adminController.MESSAGE))
+                .andExpect(flash().attribute(adminController.MESSAGE, adminController.MESSAGE_DELETE_NORMAL))
     }
 }
