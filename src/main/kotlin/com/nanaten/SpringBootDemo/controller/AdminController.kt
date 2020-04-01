0c4dd1615d348bc0d6f0e4ce64f52b4ae1d09233
m.coder <kurokoshow00@gmail.com>
2020-04-01 16:55:19 +0900

    Add Delete Article for Admin

--- src/main/kotlin/com/nanaten/SpringBootDemo/controller/AdminController.kt
+++ src/main/kotlin/com/nanaten/SpringBootDemo/controller/AdminController.kt
@@ -7,10 +7,19 @@ import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.servlet.mvc.support.RedirectAttributes
 
 @Controller
 class AdminController {
+
+    val MESSAGE_ARTICLE_DOES_NOT_EXISTS = "対象の記事が見つかりませんでした"
+    val MESSAGE = "message"
+    val MESSAGE_DELETE_NORMAL = "正常に削除しました"
+    val ALERT_CLASS_ERROR = "alert-error"
+    val ALERT_CLASS = "alert_class"
 
     val PAGE_SIZE: Int = 10
 
@@ -25,5 +34,20 @@ class AdminController {
         model.addAttribute("page", articles)
         model.addAttribute("isAdmin", true)
         return "admin_index"
+    }
+
+    @PostMapping("/admin/article/delete/{id}")
+    fun deleteArticle(@PathVariable id: Int,
+                      redirectAttributes: RedirectAttributes): String {
+        if (!articleRepository.existsById(id)) {
+            redirectAttributes.addFlashAttribute(MESSAGE, MESSAGE_ARTICLE_DOES_NOT_EXISTS)
+            redirectAttributes.addFlashAttribute(ALERT_CLASS, ALERT_CLASS_ERROR)
+            return "redirect:/admin/index"
+        }
+
+        articleRepository.deleteById(id)
+
+        redirectAttributes.addFlashAttribute(MESSAGE, MESSAGE_DELETE_NORMAL)
+        return "redirect:/admin/index"
     }
 }
