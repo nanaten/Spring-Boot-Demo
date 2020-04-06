245ca8a62789952678bd064186d52a6d6eed66dc
m.coder <kurokoshow00@gmail.com>
2020-04-06 17:41:55 +0900

    Add UserControllerTest

--- src/test/kotlin/com/nanaten/SpringBootDemo/controller/UserControllerTest.kt
+++ src/test/kotlin/com/nanaten/SpringBootDemo/controller/UserControllerTest.kt
@@ -1,86 +1,115 @@
 package com.nanaten.SpringBootDemo.controller
 
-import com.nanaten.SpringBootDemo.domain.entity.User
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
+import com.nanaten.SpringBootDemo.domain.entity.UserRole
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.MediaType
-import org.springframework.test.context.jdbc.Sql
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @SpringBootTest
+@AutoConfigureMockMvc
 internal class UserControllerTest {
-
+    @Autowired
     lateinit var mockMvc: MockMvc
 
-    @Autowired
-    lateinit var target: UserController
-
-    @BeforeEach
-    fun setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(target).build()
-    }
-
-    @AfterEach
-    fun tearDown() {
-    }
-
     @Test
-    fun addNewUser() {
-        mockMvc.perform(
-                post("/user/add")
-                        .param("name", "unit_test"))
+    fun getUserSignup() {
+        mockMvc.perform(MockMvcRequestBuilders.get("/user/signup"))
                 .andExpect(status().isOk)
-                .andExpect(content().string("Save Success."))
     }
 
     @Test
-    fun getAllUsers() {
-        mockMvc.perform(get("/user/all"))
-                .andExpect(status().isOk)
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-    }
-
-    @Test
-    @Sql(statements = ["INSERT INTO users (name) VALUES ('update_data');"])
-    fun updateUser() {
-        val lastUser: User = target.userRepository.findAll().last()
-
-        mockMvc.perform(
-                post("/user/update")
-                        .param("id", lastUser.id.toString())
-                        .param("name", "updated!!!")
+    fun postUserSignupNormal() {
+        mockMvc.perform(MockMvcRequestBuilders.post("/user/signup")
+                .with(csrf())
+                .param("name", "test1")
+                .param("email", "test1@example.com")
+                .param("password", "test1")
+                .param("role", UserRole.USER.toString())
         )
-                .andExpect(status().isOk)
-                .andExpect(content().string("Update Success."))
+                .andExpect(status().is3xxRedirection)
+                .andExpect(view().name("redirect:/user/signup"))
+
+
     }
 
-
     @Test
-    fun deleteNoUser() {
-        mockMvc.perform(
-                post("/user/delete").param("id", "-1")
+    // @Sql(statements = ["INSERT INTO users (name, email, password, role) VALUES ('test2', 'test2@example.com', 'test2', 'USER');"])
+    fun postUserSignupRegisteredError() {
+        mockMvc.perform(MockMvcRequestBuilders.post("/user/signup")
+                .with(csrf())
+                .param("name", "test2")
+                .param("email", "test2@example.com")
+                .param("password", "test2")
+                .param("role", UserRole.USER.toString())
         )
-                .andExpect(status().isOk)
-                .andExpect(content().string("User ID Unavailable."))
+                .andExpect(status().is3xxRedirection)
+                .andExpect(view().name("redirect:/user/signup"))
+                .andExpect(flash().attributeExists("errors"))
+                .andExpect(flash().attributeExists("request"))
     }
 
+    @Test
+    fun postUserSignupPasswordShort() {
+        mockMvc.perform(MockMvcRequestBuilders.post("/user/signup")
+                .with(csrf())
+                .param("name", "test2")
+                .param("email", "test2@example.com")
+                .param("password", "a")
+                .param("role", UserRole.USER.toString())
+        )
+                .andExpect(status().is3xxRedirection)
+                .andExpect(view().name("redirect:/user/signup"))
+                .andExpect(flash().attributeExists("errors"))
+                .andExpect(flash().attributeExists("request"))
+    }
 
     @Test
-    @Sql(statements = ["INSERT INTO users (name) VALUES ('delete_data');"])
-    fun deleteUser() {
-        val lastUser: User = target.userRepository.findAll().last()
-        mockMvc.perform(
-                post("/user/delete").param("id", lastUser.id.toString())
+    fun postUserSignupPasswordLong() {
+        mockMvc.perform(MockMvcRequestBuilders.post("/user/signup")
+                .with(csrf())
+                .param("name", "test2")
+                .param("email", "test2@example.com")
+                .param("password", "123456789012345")
+                .param("role", UserRole.USER.toString())
         )
-                .andExpect(status().isOk)
-                .andExpect(content().string("Delete Success."))
+                .andExpect(status().is3xxRedirection)
+                .andExpect(view().name("redirect:/user/signup"))
+                .andExpect(flash().attributeExists("errors"))
+                .andExpect(flash().attributeExists("request"))
+    }
+
+    @Test
+    fun postUserSignupPasswordCharError() {
+        mockMvc.perform(MockMvcRequestBuilders.post("/user/signup")
+                .with(csrf())
+                .param("name", "test2")
+                .param("email", "test2@example.com")
+                .param("password", "あいうえお")
+                .param("role", UserRole.USER.toString())
+        )
+                .andExpect(status().is3xxRedirection)
+                .andExpect(view().name("redirect:/user/signup"))
+                .andExpect(flash().attributeExists("errors"))
+                .andExpect(flash().attributeExists("request"))
+    }
+
+    @Test
+    fun postUserSignupAdminRegistration() {
+        mockMvc.perform(MockMvcRequestBuilders.post("/user/signup")
+                .with(csrf())
+                .param("name", "test2")
+                .param("email", "test2@example.com")
+                .param("password", "1234")
+                .param("role", UserRole.ADMIN.toString())
+        )
+                .andExpect(status().is3xxRedirection)
+                .andExpect(view().name("redirect:/user/signup"))
+                .andExpect(flash().attributeExists("errors"))
+                .andExpect(flash().attributeExists("request"))
     }
 }
