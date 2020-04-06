e7d889b452c0565a9be65f7682e9a8489178d0f6
m.coder <kurokoshow00@gmail.com>
2020-04-06 10:01:06 +0900

    Add user_login.html

--- src/main/kotlin/com/nanaten/SpringBootDemo/controller/UserController.kt
+++ src/main/kotlin/com/nanaten/SpringBootDemo/controller/UserController.kt
@@ -1,40 +1,19 @@
 package com.nanaten.SpringBootDemo.controller
 
-import com.nanaten.SpringBootDemo.domain.entity.User
 import com.nanaten.SpringBootDemo.domain.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.*
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 
-@RestController()
+@Controller
 @RequestMapping("/user")
 class UserController {
     @Autowired
     lateinit var userRepository: UserRepository
 
-    @PostMapping("/add")
-    fun addNewUser(@RequestParam name: String): String {
-        userRepository.save(User(name = name))
-        return "Save Success."
-    }
-
-    @GetMapping("/all")
-    fun getAllUsers(): Iterable<User>? {
-        return userRepository.findAll()
-    }
-
-    @PostMapping("/update")
-    fun updateUser(@RequestParam id: Int, name: String): String {
-        userRepository.save(User(id, name))
-        return "Update Success."
-    }
-
-    @PostMapping("/delete")
-    fun deleteUser(@RequestParam id: Int): String {
-        return if (userRepository.existsById(id)) {
-            userRepository.deleteById(id)
-            "Delete Success."
-        } else {
-            "User ID Unavailable."
-        }
+    @GetMapping("/login")
+    fun getUserLogin(): String {
+        return "user_login"
     }
 }
